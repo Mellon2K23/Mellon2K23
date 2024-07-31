271963aa22c6c3795597bd50a108ed13937eb60b
Mellon2K23 <122768358+Mellon2K23@users.noreply.github.com>
2024-07-31 14:14:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let secBox = document.getElementById("sec-box");
 
 // Format: Date(year, month, day, hour, minute)
 // Month is zero-based (0 for January, 1 for February, etc.)
-let endDate = new Date(2024, 8, 31, 14, 11); // August is month 7 (0-based)
+let endDate = new Date(2024, 7, 31, 14, 15); // August is month 7 (0-based)
 let endTime = endDate.getTime();
 
 function countdown() {
